0a571927c5137d9646ae5c9a0ce7eb695215d73c
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-06-06 00:17:48 +0530

    designing home page header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,13 @@
 import {useContext} from 'react';
-import {Alert, Button, Text, View} from 'react-native';
+import {
+  Alert,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import {AuthContext} from '../context/AuthContext';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../navigation/RootNavigation';
@@ -33,11 +41,65 @@ const HomeScreen: React.FC<HomeScreenProps> = ({navigation}) => {
     ]);
   };
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button title="Logout" onPress={handleLogout} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search Recipes ..."
+        />
+        <TouchableOpacity style={styles.iconBtn}>
+          <Text style={styles.iconBtnText}>+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+          <Text style={styles.logoutButtonText}>Logout</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: '#f5f5f5',
+  },
+  header: {
+    flexDirection: 'row',
+    padding: 16,
+    alignItems: 'center',
+    backgroundColor: '#007aff',
+  },
+  searchInput: {
+    flex: 1,
+    height: 45,
+    backgroundColor: '#ffffff',
+    borderRadius: 20,
+    paddingHorizontal: 16,
+    marginRight: 15,
+  },
+  iconBtn: {
+    width: 35,
+    height: 35,
+    borderRadius: 20,
+    backgroundColor: '#ffffff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  iconBtnText: {
+    fontSize: 20,
+    color: '#0071ff',
+  },
+  logoutButton: {
+    padding: 12,
+    backgroundColor: '#0b0c0a',
+    marginLeft: 12,
+    borderRadius: 24,
+  },
+  logoutButtonText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#ffffff',
+  },
+});
 
 export default HomeScreen;
